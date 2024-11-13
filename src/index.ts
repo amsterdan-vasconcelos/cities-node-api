143ea78a3e946419971d0995f79bc8a7c50f6fd1
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2024-11-13 17:37:00 -0300

    feat: create environment variables and add http-status-codes library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import { server } from './server/server';
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 server.listen(port, () => {
   console.log(`Aplicação rodando em http://localhost:${port}`);
