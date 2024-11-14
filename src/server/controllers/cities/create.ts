d2cae855d585d4d056793185536832c9886e5b68
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2024-11-14 18:47:55 -0300

    feat: add yup libraty and add createValidate function middleware

--- src/server/controllers/cities/create.ts
+++ src/server/controllers/cities/create.ts
@@ -1,9 +1,41 @@
+import * as y from 'yup';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-type Body = { name: string };
+const bodySchema = y.object().shape({
+  name: y.string().required(),
+  age: y.number().required(),
+});
+
+type Body = y.InferType<typeof bodySchema>;
 type Create = RequestHandler<unknown, unknown, Body>;
 
-export const create: Create = (_req, res) => {
-  res.status(StatusCodes.OK).json({ create: true });
+export const createValidate: RequestHandler = async (req, res, next) => {
+  try {
+    await bodySchema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    if (error instanceof y.ValidationError) {
+      const yupError = error as y.ValidationError;
+      const errors: Record<string, string> = {};
+
+      for (const { path, message } of yupError.inner) {
+        if (!path) return;
+        errors[path] = message;
+      }
+
+      res.status(StatusCodes.BAD_REQUEST).json({ errors });
+      return;
+    }
+
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Erro interno.' });
+  }
+};
+
+export const create: Create = (req, res) => {
+  const data = req.body;
+
+  res.status(StatusCodes.OK).json({ data });
 };
